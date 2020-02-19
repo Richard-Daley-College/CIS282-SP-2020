40c7199ed55d5a54ad158d583425b4930c0af0a5
sruiz113 <60885169+sruiz113@users.noreply.github.com>
2020-02-19 09:09:52 -0600

    Update DiceRoll.php

--- DiceRoll.php
+++ DiceRoll.php
@@ -1,7 +1,7 @@
 <!-- Student Name:
      Date:
 // REPLACE line #3-9 WITH INITIAL AT END OF COMMENT
-</head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" //sam
 <body>
 <?php
 $FaceNamesSingular = array("one", "two", "three", "four", "five", "six");
@@ -11,12 +11,12 @@ $FaceNamesSingular = array("one", "two", "three", "four", "five", "six");
           echo "The roll was double ", $FaceNamesPlural[$Die1-1], ".<br />";
           $ReturnValue = true;
      }
-     else { // Not Doubles
+$FaceNamesPlural = array("ones", "twos", "threes", "fours", "fives", "sixes"); //sam
 // REPLACE line #27-33 WITH INITIAL AT END OF COMMENT
 
-function DisplayScoreText($Score, $Doubles) {
+function CheckForDoubles($Die1, $Die2) { //sam
      switch ($Score) {
-          case 2:
+     global $FaceNamesPlural; //sam
                echo "You rolled snake eyes!<br />";
                break;
 // REPLACE line #41-46 WITH INITIAL AT END OF COMMENT
@@ -27,10 +27,10 @@ function DisplayScoreText($Score, $Doubles) {
                break;
           case 11:
 // REPLACE Line #53-59 WITH INITIAL AT END OF COMMENT
-                                      number */
-                    if ($Doubles) {
-                         echo "You rolled a hard
-                              $Score!<br />";
+                                      number *
+ //sam                                          
+     return $ReturnValue; //sam
+}  //sam 
 // REPLACE Line #64-70 WITH INITIAL AT END OF COMMENT
      }
 }
@@ -40,10 +40,10 @@ $RollCount = 0;
 $ScoreCount = array();
 for ($PossibleRolls = 2; $PossibleRolls <= 12; ++$PossibleRolls) {
      $ScoreCount[$PossibleRolls] = 0;
-}
+     case 5: //sam
 
 foreach ($FaceValues as $Die1) {
-     foreach ($FaceValues as $Die2) {
+     case 7: //sam
           ++$RollCount;
           $Score = $Die1 + $Die2;
           ++$ScoreCount[$Score];
