410a91e8acdccc8937f22f67de922a20e8004c53
tdavis446 <60886840+tdavis446@users.noreply.github.com>
2020-02-24 08:40:49 -0600

    Update DiceRoll.php

--- DiceRoll.php
+++ DiceRoll.php
@@ -1,25 +1,49 @@
 <!-- Student Name:
      Date:
-// REPLACE line #3-9 WITH INITIAL AT END OF COMMENT
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <title>Dice Roll</title>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
 <?php
 $FaceNamesSingular = array("one", "two", "three", "four", "five", "six");
-// REPLACE line #14-20 WITH INITIAL AT END OF COMMENT
+$FaceNamesPlural = array("ones", "twos", "threes", "fours", "fives", "sixes");
+$FaceValues = array( 1, 2, 3, 4, 5, 6); 
 
+function CheckForDoubles($Die1, $Die2){ 
+	global $FaceNamesSingular; 
+	global $FaceNamesPlural;
+	$ReturnValue = false; 
+	
      if ($Die1 == $Die2) {// Doubles
           echo "The roll was double ", $FaceNamesPlural[$Die1-1], ".<br />";
           $ReturnValue = true;
      }
      else { // Not Doubles
-// REPLACE line #27-33 WITH INITIAL AT END OF COMMENT
-
+	 echo "The roll was a ", $FaceNamesSingular[$Die1-1 ],
+	 " and a ", $FaceNamesSingular[$Die2-1], ".<br />"; 
+	 $ReturnValue = false; 
+	 }
+	 
+	 return $ReturnValue;
+}
+	 
 function DisplayScoreText($Score, $Doubles) {
      switch ($Score) {
           case 2:
                echo "You rolled snake eyes!<br />";
                break;
-// REPLACE line #41-46 WITH INITIAL AT END OF COMMENT
+          case 3:
+               echo "You rolled a loose deuce!<br />"; 
+			   break;
+          case 5:
+               echo "You rolled a fever five!<br />"; 
+			   break;
+          case 7:  
                echo "You rolled a natural!<br />";
                break;
           case 9:
