cca16a9562b577a2b9e685d8e14f907c81d9c171
tdavis446 <60886840+tdavis446@users.noreply.github.com>
2020-02-24 09:15:47 -0600

    Revert "Merge pull request #1 from Richard-Daley-College/sruiz113-patch-1"
    This reverts commit 73761f6dd14e3b60bc72647d0a88476e56f04083, reversing
    changes made to dd241da9b2d7dc3e889082bc7fae843b19fd050c.

--- DiceRoll.php
+++ DiceRoll.php
@@ -1,6 +1,13 @@
 <!-- Student Name: Taia
      Date:
-
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <title>Dice Roll</title>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+</head>
 <body>
 <?php
 $FaceNamesSingular = array("one", "two", "three", "four", "five", "six");
@@ -16,9 +23,18 @@ function CheckForDoubles($Die1, $Die2){
           echo "The roll was double ", $FaceNamesPlural[$Die1-1], ".<br />";
           $ReturnValue = true;
      }
-
+     else { // Not Doubles
+	 echo "The roll was a ", $FaceNamesSingular[$Die1-1 ],
+	 " and a ", $FaceNamesSingular[$Die2-1], ".<br />"; 
+	 $ReturnValue = false; 
+	 }
+	 
+	 return $ReturnValue;
+}
+	 
+function DisplayScoreText($Score, $Doubles) {
      switch ($Score) {
-     global $FaceNamesPlural; //sam
+          case 2:
                echo "You rolled snake eyes!<br />";
                break;
           case 3:
@@ -35,10 +51,10 @@ function CheckForDoubles($Die1, $Die2){
                break;
           case 11:
 // REPLACE Line #53-59 WITH INITIAL AT END OF COMMENT
-                                      number *
- //sam                                          
-     return $ReturnValue; //sam
-}  //sam 
+                                      number */
+                    if ($Doubles) {
+                         echo "You rolled a hard
+                              $Score!<br />";
 // REPLACE Line #64-70 WITH INITIAL AT END OF COMMENT
      }
 }
@@ -48,10 +64,10 @@ $RollCount = 0;
 $ScoreCount = array();
 for ($PossibleRolls = 2; $PossibleRolls <= 12; ++$PossibleRolls) {
      $ScoreCount[$PossibleRolls] = 0;
-     case 5: //sam
+}
 
 foreach ($FaceValues as $Die1) {
-     case 7: //sam
+     foreach ($FaceValues as $Die2) {
           ++$RollCount;
           $Score = $Die1 + $Die2;
           ++$ScoreCount[$Score];
